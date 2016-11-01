41a6f3680240d542824237f6454e4e8537577a52
Rhys Henson <rhysh@live.co.uk>
2016-11-01 17:54:12 +0000

    Site selection added, currently displays names and icons and nothing more

--- RTScraper/MainWindow.xaml.cs
+++ RTScraper/MainWindow.xaml.cs
@@ -22,7 +22,31 @@ namespace RTScraper
     {
         public MainWindow()
         {
+            List<RTSites> RTList = new List<RTSites>();
+            RTList.Add(new RTSites("Rooster Teeth", "https://roosterteeth.com/rt-favicon.png"));
+            RTList.Add(new RTSites("Achievement Hunter", "https://achievementhunter.roosterteeth.com/ah-favicon.png"));
+            RTList.Add(new RTSites("Funhaus", "https://funhaus.roosterteeth.com/fh-favicon.png"));
+            RTList.Add(new RTSites("ScrewAttack", "https://screwattack.roosterteeth.com/sa-favicon.png"));
+            RTList.Add(new RTSites("Game Attack", "https://gameattack.roosterteeth.com/ga-favicon.png"));
+            RTList.Add(new RTSites("The Know", "https://theknow.roosterteeth.com/tk-favicon.png"));
+            RTList.Add(new RTSites("Cow Chop", "https://cowchop.roosterteeth.com/cc-favicon.png"));
+
             InitializeComponent();
+
+
+            RTSitesList.ItemsSource = RTList;
+        }
+    }
+
+    public class RTSites
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+        public RTSites(string Name, string Image)
+        {
+            this.Name = Name;
+            this.Image = Image;
         }
     }
 }
